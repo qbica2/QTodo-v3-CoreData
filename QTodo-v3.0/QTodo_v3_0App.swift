21ae17951bd44ba045ef75d42e4eaf020dc3e2f4
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-15 15:01:22 +0300

    Initial Commit

---
+++ QTodo-v3.0/QTodo_v3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  QTodo_v3_0App.swift
+//  QTodo-v3.0
+//
+//  Created by Mehmet Kubilay Akdemir on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct QTodo_v3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
