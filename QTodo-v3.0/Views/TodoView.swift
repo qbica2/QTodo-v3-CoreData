7d3f134da02813d79d2285bc67fcf45e89abe5f5
Kubilay Akdemir <kubidik27@gmail.com>
2023-06-15 18:26:29 +0300

    create TodoView

---
+++ QTodo-v3.0/Views/TodoView.swift
@@ -0,0 +1,42 @@
+//
+//  TodoView.swift
+//  QTodo-v3.0
+//
+//  Created by Mehmet Kubilay Akdemir on 15.06.2023.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    
+    let todo: TodoModel
+    
+    var body: some View {
+        NavigationLink(value: todo) {
+            HStack{
+                Image(systemName: todo.isCompleted ? "circle.fill" : "circle")
+                Text(todo.title)
+                
+                if let dueDate = todo.dueDate {
+                    Spacer()
+                    Text(formatDate(dueDate))
+                        .foregroundColor(.gray)
+                }
+            }
+            .font(.headline)
+            .foregroundColor(.pink.opacity(0.8))
+        }
+    }
+    
+    private func formatDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM d, yyyy"
+        return formatter.string(from: date)
+    }
+}
+
+struct TodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoView(todo: TodoModel(title: "Second Todo", description: "Çalış", isCompleted: true, dueDate: nil))
+    }
+}
